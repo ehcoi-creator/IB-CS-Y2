e0ba1ee1b3ee8bd76e0eba07dd512f98dd9092e1
ehcoi-creator <euihyeonchoi@gmail.com>
2026-08-24 06:12:47 +0000

    Complete checker

--- JIS/QUIZ/testing.java
+++ JIS/QUIZ/testing.java
@@ -1,6 +1,11 @@
 class testing{
   public static void main(String[] args) {
-    IPv4Gen addr = new IPv4Gen();
-    System.out.println(addr.generate());
+    checker test1 = new checker("hello");
+    System.out.println(test1.test);
+    //False
+    checker test2 = new checker("192.45.7.201");
+    System.out.println(test2.test);
+    //True
+
   }
 }
